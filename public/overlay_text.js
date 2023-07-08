772ec0c09112f379335aeb3e194cebe273bb49c3
adam-stone <25stonea@charlottelatin.org>
2023-07-08 21:05:03 +0900

    documentation link added

--- public/overlay_text.js
+++ public/overlay_text.js
@@ -4,17 +4,23 @@ const createOverlayText = () => {
 
 const createdBy = () => {
     const xPos = 10; // 10px from the left edge
-    const yPos = height - 10; // 10px from the bottom edge
-    svg.append("text")
-        .attr("font-size", "14px")
-        .attr("x", xPos)
-        .attr("y", yPos)
-        .attr("class", "overlay-text")
-        .text("Created by ")
-        .append("a")
-        .attr("xlink:href", "https://fabacademy.org/2023/labs/charlotte/students/adam-stone/")
-        .attr("target", "_blank") 
-        .style("fill", "#aaaaff")
-        .attr("font-size", "14px")
-        .text("Adam Stone");
+    
+    const createTxt = (ySub, txt, lnk_txt, lnk) => {
+        const yPos = height - ySub; // 10px from the bottom edge
+        svg.append("text")
+            .attr("font-size", "14px")
+            .attr("x", xPos)
+            .attr("y", yPos)
+            .attr("class", "overlay-text")
+            .text(txt + " ")
+            .append("a")
+            .attr("xlink:href", lnk)
+            .attr("target", "_blank") 
+            .style("fill", "#aaaaff")
+            .attr("font-size", "14px")
+            .text(lnk_txt);
+    };
+
+    createTxt(30, "Created by", "Adam Stone", "https://fabacademy.org/2023/labs/charlotte/students/adam-stone/")
+    createTxt(10, "For documentation", "click here", "https://fabacademy.org/2023/labs/charlotte/students/adam-stone/lessons/side-projects/lab-link-graph/")
 };
