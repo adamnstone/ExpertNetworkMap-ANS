664eee842c2308b60b200e4ee6545166a6f7dcb9
adam-stone <25stonea@charlottelatin.org>
2023-12-06 21:51:45 -0500

    overlay text link altered

--- public/overlay_text.js
+++ public/overlay_text.js
@@ -21,9 +21,24 @@ const createdBy = () => {
             .text(lnk_txt);
     };
 
-    createTxt(65, "Created by", "Adam Stone", "https://fabacademy.org/2023/labs/charlotte/students/adam-stone/")
-    createTxt(50, "To join the Mattermost channel", "click here", "https://chat.academany.org/fabacademy-2023/channels/fab-academy-data-viz");
+    createTxt(50, "Created by", "Adam Stone", "https://adamnstone.com")
+    //createTxt(50, "To join the Mattermost channel", "click here", "https://chat.academany.org/fabacademy-2023/channels/fab-academy-data-viz");
     createTxt(35, "For documentation", "click here", "https://gitlab.fabcloud.org/pub/project/expert-network-map/-/blob/main/documentation.md")
     createTxt(20, "Took inspiration from", "Nadieh Bremer", "https://royalconstellations.visualcinnamon.com/")
     createTxt(5, "Recieved guidance and support from", "Francisco Sanchez", "https://www.fablabs.io/users/francisco")
+
+    svg.append("text")
+        .attr("font-size", "30px")
+        .attr("x", width / 2)
+        .attr("y", height - 10)
+        .attr("text-anchor", "middle")
+        .attr("class", "overlay-text")
+        .style("fill", "red")
+        .text("Join the Mattermost Channel ")
+        .append("a")
+        .attr("xlink:href", "https://chat.academany.org/fabacademy-2023/channels/fab-academy-data-viz")
+        .attr("target", "_blank") 
+        .style("fill", OVERLAY_TEXT_LINK_COLOR)
+        .attr("font-size", "30px")
+        .text("Here");
 };
